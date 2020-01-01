a5b161e6985346a135ead7df9e0d5ebb8e6bea24
sandraskars <sandraskarshaug@gmail.com>
2020-01-01 21:27:08 +0100

    Add endpoints for deleting/adding subjects and themes in AdminEndpoint
    Create Subject class
    Create Theme class

---
+++ src/main/java/no/capraconsulting/domain/Theme.java
@@ -0,0 +1,10 @@
+package no.capraconsulting.domain;
+
+public class Theme {
+    public String themeTitle;
+    public int subjectId;
+
+    public String getThemeTitle() { return themeTitle; }
+
+    public int getSubjectId() { return subjectId; }
+}
